04d43db9af53fa50a539760aba1245d2a436c7a2
Luis Requena <requenalc@gmail.com>
2024-12-08 13:43:18 -0300

    fix de ventas

--- venta_analisis.py
+++ venta_analisis.py
@@ -6,18 +6,18 @@ from utilitarios import calculate_difference_and_time, remove_code_delimiters
 from binance_functions import get_symbol_value_in_usdt, sell_crypto
 from database_functions import process_sale
 actives = get_active_transactions_details()
-
 for sym in actives:
     indicator = get_technical_indicators(sym["simbolo"])
     info = calculate_difference_and_time(sym, indicator)
     sym["PORCENTAJE_DE_GANANCIA"] = info["porcentaje_diferencia"]
     sym["TIEMPO_TRANSCURRIDO_DESDE_LA_COMPRA"] = info["tiempo_transcurrido"]
+    
     balance = get_symbol_value_in_usdt(sym["simbolo"].split('USDT')[0])
 
     prompt = f"""
                 como experto en tradding de crypto divisas realiza un analisis de venta del siguiente simbolos e indicame si es viable vender o esperar actualmente tu balance para el simbolo {sym["simbolo"]} es
                 {balance} tu obejtivo es maximizar tus ganancias y reducir perdidas para lograr la meta establecida en la base de conocimientos 
-                analiza los suientes datos antes de decidir no te apresures a vender si no tienes una señal clara de venta
+                analiza los siguientes datos antes de decidir no te apresures a vender si no tienes una señal clara de venta solo debes responder en el formato que explica en la base de conocimientos
                 {sym}
                 """
     recom = get_gpt_recommendation(prompt)
